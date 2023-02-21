ae081333fef12fe5eeefac8e69b1831c18b9f178
Yura <yuratezher@mail.ru>
2023-02-21 14:45:24 +0300

    test

--- js/main.js
+++ js/main.js
@@ -406,3 +406,102 @@ $('.del-ed').on('click', function () {
     $('.popUp-rate').addClass("active")
     $('.substrate').addClass("active")
 });
+
+const subscriptionSwitch = document.querySelector(".subscription-switch")
+
+if(subscriptionSwitch){
+    subscriptionSwitch.addEventListener("click",()=>{
+        let target = event.target
+        if(target.classList.contains("subscription__btn--current")){
+            $('.subscription-switch__btn').removeClass("active");
+            $('.history-subscription').removeClass("active");
+            $('.subscription__btn--current').addClass("active");
+            $('.profile-current-subscription').addClass("active");
+        }else if(target.classList.contains("subscription__btn--history")){
+            $('.subscription-switch__btn').removeClass("active");
+            $('.profile-current-subscription').removeClass("active");
+            $('.subscription__btn--history').addClass("active");
+            $('.history-subscription').addClass("active");
+        }
+    })
+}
+
+
+$('.referral-link-btn').on('click', function () {
+    let copyText = document.querySelector(".referral-program__link");
+    copyText.value;
+    navigator.clipboard.writeText( copyText.value)
+});
+
+
+
+/*$('#calendar').dateRangePicker({
+    language: {
+        days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+        daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
+        daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthsShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+        today: 'Сегодня',
+        clear: 'Очистить',
+        dateFormat: 'dd.mm.yyyy',
+        timeFormat: 'hh:ii',
+        firstDay: 1
+    },
+    separator : ' to ',
+    position: "left top",
+    getValue: function()
+    {
+        if ($('#date-range200').val() && $('#date-range201').val() )
+            return $('#date-range200').val() + ' to ' + $('#date-range201').val();
+        else
+            return '';
+    },
+    setValue: function(s,s1,s2)
+    {
+        $('#date-range200').val(s1);
+        $('#date-range201').val(s2);
+    }
+}	)*/
+
+
+let dpMin, dpMax;
+
+dpMin = new AirDatepicker('#el1', {
+    onSelect({date}) {
+        dpMax.update({
+            minDate: date
+        })
+    }
+})
+
+dpMax = new AirDatepicker('#el2', {
+    onSelect({date}) {
+        dpMin.update({
+            maxDate: date
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
